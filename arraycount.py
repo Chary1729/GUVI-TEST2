99c2b2c61d91181714e8a63b7a11c5ef31c6e100
uday kumar mailaram <39719416+Chary1729@users.noreply.github.com>
2019-03-23 18:30:32 +0530

    Create arraycount.py

---
+++ arraycount.py
@@ -0,0 +1,12 @@
+N,K=int(input()),int(input())
+print(N,K)
+hello_array=[]
+for i in range(1,N+1):
+  hello_array.append(i)
+print(hello_array)
+sum=0
+for i in my_honor[0:K]:
+  sum=sum+i
+print(sum)
+  
+  
