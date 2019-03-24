4f46ed726f2cd80af362b89eb52dcf9da2bc2ec4
uday kumar mailaram <39719416+Chary1729@users.noreply.github.com>
2019-03-24 22:38:55 +0530

    Update and rename evennums.py to evennumbers.py

--- evennumbers.py
+++ evennumbers.py
@@ -3,7 +3,7 @@ if(N.isdigit()==True and k.isdigit()==True):
   N=int(N)
   k=int(k)
   count=0
-  for i in range(N,k):
+  for i in range(N+1,k):
     if(i%2==0):
       count=count+1
       print(i,end=' ')
