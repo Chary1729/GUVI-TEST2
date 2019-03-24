4f8c0a677dc550fadfe98973ec71850ae811afe6
uday kumar mailaram <39719416+Chary1729@users.noreply.github.com>
2019-03-24 23:26:13 +0530

    Create primecount.py

---
+++ primecount.py
@@ -0,0 +1,11 @@
+N,k=input().split()
+if(N.isdigit()==True and k.isdigit()==True):
+  N=int(N)
+  k=int(k)
+for val in range(N+1, k - 1): 
+   if val > 1:
+      for n in range(2, val):
+        if (val % n) == 0:
+          break
+      else:
+        print(val) 
