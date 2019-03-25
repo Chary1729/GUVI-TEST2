652d9732f5d20757abbd67491fb2bdbe28f558ab
uday kumar mailaram <39719416+Chary1729@users.noreply.github.com>
2019-03-25 21:20:01 +0530

    Create armstro.py

---
+++ armstro.py
@@ -0,0 +1,12 @@
+num = int(input())
+if(num<=100000):
+  sum = 0
+  temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+if num == sum:
+   print("yes")
+else:
+   print("no")
